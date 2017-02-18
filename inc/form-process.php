40a7fc001dd4ab9e9fac118f5a9c1636429a2206
mozadeh <mo@sidebuy.com>
2017-02-18 08:52:29 -0500

    first push

---
+++ inc/form-process.php
@@ -0,0 +1,34 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$email_to = "demo@domain.com";
+$subject = "New Message Received";
+
+if($name != '' && $email != '' && $message != ''){
+    // prepare email body text
+    $body = "";
+    $body .= "Name: ";
+    $body .= $name;
+    $body .= "\n";
+    $body .= "Email: ";
+    $body .= $email;
+    $body .= "\n";
+    $body .= "Message: ";
+    $body .= $message;
+    $body .= "\n";
+
+    // send email
+    $success = mail($email_to, $subject, $body, "From:".$email);
+
+    // sending back message status
+    if ($success){
+        echo "success";
+    }else{
+        echo "Something went wrong!";
+    }
+}else{
+    echo "You Must Not Left Anything Empty!";
+}
+?>
